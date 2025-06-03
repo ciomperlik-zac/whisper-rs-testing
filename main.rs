6a527cb6fe9fce9dcc582bf886cbaddcfda866e1
ciomperlik-zac <148258873+ciomperlik-zac@users.noreply.github.com>
2025-06-02 19:36:09 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,12 @@
+mod whisper;
+use whisper::SttEngine;
+use windows_sys;
+
+fn main() {
+    let stt_engine =
+        SttEngine::new("C:\\Users\\zciom\\Desktop\\Rust\\assistant\\whisper\\ggml-base.en.bin");
+
+    let output_sigma = stt_engine.transcribe_wav("C:\\Users\\zciom\\output.wav");
+
+    println!("{}", output_sigma);
+}
