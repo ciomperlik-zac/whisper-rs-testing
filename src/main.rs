eef59497d5a21cfc4f183ddb74e4aa0638c25147
ciomperlik-zac <148258873+ciomperlik-zac@users.noreply.github.com>
2025-06-02 19:39:26 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let stt_engine =
         SttEngine::new("C:\\Users\\zciom\\Desktop\\Rust\\assistant\\whisper\\ggml-base.en.bin");
 
-    let output_sigma = stt_engine.transcribe_wav("C:\\Users\\zciom\\output.wav");
+    let output = stt_engine.transcribe_wav("C:\\Users\\zciom\\output.wav");
 
-    println!("{}", output_sigma);
+    println!("{}", output);
 }
